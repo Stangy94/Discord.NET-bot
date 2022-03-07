2cdf8f23a66468a9000d2f11d9b158966ed50e71
Stangy94 <stangroomjack@gmail.com>
2022-03-07 19:20:02 +0000

    initial code commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Discord;
+
+namespace DNet_V3_Tutorial
+{
+    public class program
+    {
+
+        public static Task Main(string[] args) => new program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+            services
+
+            .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
+            {
+                GatewayIntents = Discord.GatewayIntents.AllUnprivileged,
+                AlwaysDownloadUsers = true,
+            })))
+            .Build();
+
+            await RunAsync(host);
+        }
+
+        public async Task RunAsync(IHost host)
+        {
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            var _client = provider.GetRequiredService<DiscordSocketClient>();
+
+            _client.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
+
+            _client.Ready += async () =>
+            {
+                Console.WriteLine("Bot ready!");
+            };
+
+
+            await _client.LoginAsync(Discord.TokenType.Bot, "Insert Bot Token Here");
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
