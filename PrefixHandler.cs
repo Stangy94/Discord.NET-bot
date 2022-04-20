a3415dd8f7dc7a21be24fe4d6adc88cdcf44eb08
Stangy94 <stangroomjack@gmail.com>
2022-04-20 19:57:11 +0100

    Slash commands and modals

---
+++ PrefixHandler.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace C_Bot
+{
+    public class PrefixHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly IConfiguration _config;
+
+
+    public PrefixHandler(DiscordSocketClient client, CommandService commands, IConfigurationRoot config)
+    {
+       _client = client;
+       _commands = commands;
+       _config = config;
+    }
+        public async Task InitializeAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        public void  AddModule<T>()
+        {
+            _commands.AddModuleAsync<T>(null);
+
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!(message.HasCharPrefix(_config["prefix"][0], ref argPos) || !message.HasMentionPrefix(_client.CurrentUser, ref argPos)) || message.Author.IsBot) return;
+
+            var context = new SocketCommandContext(_client, message);
+
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿using Discord.WebSocket;
+﻿using Discord.Interactions;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration.Yaml;
 using Discord;
 
-namespace DNet_V3_Tutorial
+namespace C_Bot
 {
     public class program
     {
@@ -12,15 +16,26 @@ namespace DNet_V3_Tutorial
 
         public async Task MainAsync()
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddYamlFile("config.yml")
+                .Build();
+
             using IHost host = Host.CreateDefaultBuilder()
                 .ConfigureServices((_, services) =>
             services
+            .AddSingleton(config)
 
             .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
             {
-                GatewayIntents = Discord.GatewayIntents.AllUnprivileged,
+                GatewayIntents = Discord.GatewayIntents.All,
                 AlwaysDownloadUsers = true,
-            })))
+            }))
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<InteractionHandler>()
+            .AddSingleton(x => new CommandService())
+            .AddSingleton<PrefixHandler>()
+            )
             .Build();
 
             await RunAsync(host);
@@ -32,16 +47,23 @@ namespace DNet_V3_Tutorial
             IServiceProvider provider = serviceScope.ServiceProvider;
 
             var _client = provider.GetRequiredService<DiscordSocketClient>();
+            var sCommands = provider.GetRequiredService<InteractionService>();
+            await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
+            var config = provider.GetRequiredService<IConfigurationRoot>();
+            var pCommands = provider.GetRequiredService<PrefixHandler>();
+            pCommands.AddModule<C_Bot.Modules.Prefix_Module>();
+            await pCommands.InitializeAsync();
 
             _client.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
-
+            sCommands.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
             _client.Ready += async () =>
             {
                 Console.WriteLine("Bot ready!");
+                await sCommands.RegisterCommandsToGuildAsync(UInt64.Parse(config["testGuild"]));
             };
 
 
-            await _client.LoginAsync(Discord.TokenType.Bot, "Insert Bot Token Here");
+            await _client.LoginAsync(Discord.TokenType.Bot, config["tokens:discord"]);
             await _client.StartAsync();
 
             await Task.Delay(-1);
